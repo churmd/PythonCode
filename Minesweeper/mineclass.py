86401539d65869a2aba1da8f119714a2fd8dfb05
churmd <churmdaniel@gmail.com>
2018-06-18 16:25:32 +0100

    Changed button/cell lists to dicts. Changed surCoordinates name and made it check for any surrounding mines

--- Minesweeper/mineclass.py
+++ Minesweeper/mineclass.py
@@ -3,10 +3,10 @@ import tkinter
 import random
 
 class Cell(tkinter.Button):
-    def __init__(self, master, symbol=" "):
+    def __init__(self, master, row, col, symbol=" "):
         tkinter.Button.__init__(self, master)
         self.config(text = " ", command = self.reveal, bg = "blue")
-        self.pack()
+        self.grid(row=row, column=col)
         self.symbol = symbol
 
     def reveal(self):
@@ -15,22 +15,30 @@ class Cell(tkinter.Button):
 
 class Game(tkinter.Tk):
     def __init__(self):
-            tkinter.Tk.__init__(self)
-            self.buttons = [Cell(self, "Value " + str(i)) for i in range(10)]
-            self.boardSize = 8
-            self.mineNum = 10
-            self.board = []
+        tkinter.Tk.__init__(self)
+        self.bSize = 8
+        self.numMine = 10
+        self.cellDict = {}
+        self.buttons = {}
+        self.newBoard()
 
-    def newBoard(self):
-        board = [[0] * self.boardSize for _ in self.boardSize]
-        mines = random.sample(range(self.boardSize*self.boardSize), self.mineNum)
-        for m in mines:
-            row, col = divmod(self.boardSize, m)
-            board[row][column] = -1
-            #TODO update surrounding coords
-
-    def getSurCoords(self, row, col):
+    def surCells(self, coord):
+        (row, col) = coord
         rows = range(row-1, row+2)
         cols = range(col-1, col+2)
-        inBounds = lambda x,y : x>0 and y>0 and x<self.boardSize and y<self.boardSize
-        coords = [(x,y) for x in row for y in cols if inBounds(x,y)]
+        inBounds = lambda x,y : x>=0 and y>=0 and x<self.bSize and y<self.bSize
+        isMine = lambda x,y : self.cellDict[(x,y)] == -1
+        sur = [(x,y) for x in rows for y in cols if inBounds(x,y) and not isMine(x,y)]
+        return sur
+
+    def newBoard(self):
+        sz = range(self.bSize)
+        self.cellDict = {(x,y) : 0 for x in sz for y in sz}
+        mines = random.sample(range(self.bSize*self.bSize), self.numMine)
+        for m in mines:
+            cell = divmod(m, self.bSize)
+            self.cellDict[cell] = -1
+            for sur in self.surCells(cell):
+                self.cellDict[sur] += 1
+        makeButton = lambda x,y : Cell(self,x,y,str(self.cellDict[(x,y)]))
+        self.buttons = {(x,y) : makeButton(x,y) for x in sz for y in sz}
